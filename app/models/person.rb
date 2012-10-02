08ca4c256aaee945d4ca7784e3fdcbd71b6b3377
zeschmidt <ze.schmidt@gmail.com>
2012-10-02 17:53:44 -0300

    first commit

---
+++ app/models/person.rb
@@ -0,0 +1,3 @@
+class Person < ActiveRecord::Base
+  attr_accessible :nome
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Verdadeiro::Application.config.secret_token = 'be484f52b61660f0f29b0e55a5fd80e0ef10e30c7cbbc94cd12c221d40f6c16a8a0e18fca731f0f0d7cb197a04fcfc7c8cf2b76597cfe8b3b6c7442195253302'
